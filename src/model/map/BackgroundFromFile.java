0ccd7e43b8a07ec3f5b0d6b81dfab159ba3d1280
Anis <anis.lico@outlook.it>
2018-08-09 20:41:28 +0200

    fix error filereader

--- src/model/map/BackgroundFromFile.java
+++ src/model/map/BackgroundFromFile.java
@@ -1,6 +1,9 @@
 package model.map;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -8,10 +11,10 @@ import model.room.RoomType;
 
 public enum BackgroundFromFile {
 
-	FIRST("rooms/1.txt", RoomType.INTERMEDIATE, 1), SECOND("rooms/2.txt", RoomType.INTERMEDIATE, 2),
+	 ZERO("rooms/0.txt", RoomType.VENDOR, 0), FIRST("rooms/1.txt", RoomType.INTERMEDIATE, 1), SECOND("rooms/2.txt", RoomType.INTERMEDIATE, 2),
 	THIRD("rooms/3.txt", RoomType.INTERMEDIATE, 3), FOURTH("rooms/4.txt", RoomType.INTERMEDIATE, 4),
-	FIVTH("rooms/5.txt", RoomType.INTERMEDIATE, 5), SIXTH("rooms/6.txt", RoomType.BOSS, 6),
-	SEVENTH("rooms/7.txt", RoomType.BOSS, 7), EIGHTH("rooms/8.txt", RoomType.BOSS, 8), NINETH("rooms/9.txt", RoomType.VENDOR, 9);
+	FIVTH("rooms/5.txt", RoomType.INTERMEDIATE, 5), SIXTH("rooms/6.txt", RoomType.INTERMEDIATE, 6),
+	SEVENTH("rooms/7.txt", RoomType.BOSS, 7), EIGHTH("rooms/8.txt", RoomType.BOSS, 8), NINETH("rooms/9.txt", RoomType.BOSS, 9);
 
 	private final String path;
 	private final RoomType type;
@@ -26,16 +29,22 @@ public enum BackgroundFromFile {
 	public  static String getRandomPath(RoomType type) {
 		int seq  = type.equals(RoomType.INTERMEDIATE) ? new Random().nextInt(5) + 1 : 
 						type.equals(RoomType.VENDOR) ? 9 : 0 ;
-		return getPath(seq);
+		return getPathName(seq);
 	}
 
 
-	private static String getPath(int seqNumber) {
+	private static String getPathName(int seqNumber) {
 		return seqNumber == 1 ? FIRST.getPath() : seqNumber == 2 ? SECOND.getPath() : seqNumber == 3 ? THIRD.getPath() : 
-					seqNumber == 4 ? FOURTH.getPath() : seqNumber == 5 ? FIVTH.getPath() : seqNumber == 9 ? NINETH.getPath() : "" ;
+					seqNumber == 4 ? FOURTH.getPath() : seqNumber == 5 ? FIVTH.getPath() : seqNumber == 9 ? ZERO.getPath() : FIRST.getPath();
 	}
 
 	public String getPath() {
 		return this.path;
+	}
+	
+	public static List<String> getBossPath(){
+		List<String> listCopy  = Arrays.asList(BackgroundFromFile.SIXTH.getPath(), 
+				BackgroundFromFile.SEVENTH.getPath(), BackgroundFromFile.EIGHTH.getPath());
+		return listCopy;
 	}
 }
