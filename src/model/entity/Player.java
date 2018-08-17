c7cb427bf2247810b4663f67b52451795d200884
Tommi <tommasoghini@hotmail.it>
2018-08-17 15:10:41 +0200

    change some variables

--- src/model/entity/Player.java
+++ src/model/entity/Player.java
@@ -100,7 +100,7 @@ public enum Player {
 
         @Override
         public int shootingDamage() {
-            return 90;
+            return 110;
         }
     },
 
@@ -130,7 +130,7 @@ public enum Player {
 
         @Override
         public int getStartingMaxLife() {
-            return 18;
+            return 17;
         }
 
         @Override
@@ -145,7 +145,7 @@ public enum Player {
 
         @Override
         public int shootingDamage() {
-            return 180;
+            return 200;
         }
     },
 
