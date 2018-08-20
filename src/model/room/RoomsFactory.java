6350e3df1e755c516550ba208d3631daadd86172
Simone Del Gatto <simone.delgatto@studio.unibo.it>
2018-08-20 13:06:55 +0200

    fix

--- src/model/room/RoomsFactory.java
+++ src/model/room/RoomsFactory.java
@@ -7,60 +7,56 @@ package model.room;
  */
 public interface RoomsFactory {
 
-	/**
-	 * Create a vendor Room
-	 * 
-	 * @param roomID
-	 * 	
-	 * @param isCompleted
-	 * 	
-	 * @param isVisited
-	 * 	
-	 * @return Room
-	 * 			Vendor Room
-	 */
-	Room vendorRoom(int roomID, boolean isCompleted, boolean isVisited);
+    /**
+     * Create a vendor Room.
+     * 
+     * @param roomID
+     * 
+     * @param isCompleted
+     * 
+     * @param isVisited
+     * 
+     * @return Room Vendor Room
+     */
+    Room vendorRoom(int roomID, boolean isCompleted, boolean isVisited);
 
-	/**
-	 * Create a first Room
-	 * 
-	 * @param roomID
-	 * 	
-	 * @param isCompleted
-	 * 	
-	 * @param isVisited
-	 * 	
-	 * @return Room
-	 * 			First Room
-	 */
-	Room FirstRoom(int roomID, boolean isCompleted, boolean isVisited);
-	
-	/**
-	 * Create a Intermediate Room
-	 * 
-	 * @param roomID
-	 * 	
-	 * @param isCompleted
-	 * 	
-	 * @param isVisited
-	 * 	
-	 * @return Room
-	 * 			Intermediate Room
-	 */
-	Room IntermediateRoom(int roomID, boolean isCompleted, boolean isVisited);
-	
-	/**
-	 * Create a Boss Room
-	 * 
-	 * @param roomID
-	 * 	
-	 * @param isCompleted
-	 * 	
-	 * @param isVisited
-	 * 	
-	 * @return Room
-	 * 			Boss Room
-	 */
-	Room BossRoom(int roomID, boolean isCompleted, boolean isVisited);
+    /**
+     * Create a first Room.
+     * 
+     * @param roomID
+     * 
+     * @param isCompleted
+     * 
+     * @param isVisited
+     * 
+     * @return Room First Room
+     */
+    Room FirstRoom(int roomID, boolean isCompleted, boolean isVisited);
+
+    /**
+     * Create a Intermediate Room.
+     * 
+     * @param roomID
+     * 
+     * @param isCompleted
+     * 
+     * @param isVisited
+     * 
+     * @return Room Intermediate Room
+     */
+    Room IntermediateRoom(int roomID, boolean isCompleted, boolean isVisited);
+
+    /**
+     * Create a Boss Room.
+     * 
+     * @param roomID
+     * 
+     * @param isCompleted
+     * 
+     * @param isVisited
+     * 
+     * @return Room Boss Room
+     */
+    Room BossRoom(int roomID, boolean isCompleted, boolean isVisited);
 
 }
